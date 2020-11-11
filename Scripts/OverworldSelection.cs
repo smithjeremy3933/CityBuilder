4446fd7b3c537184b68edefed260f35b2d4c8cd8
smithjeremy3933@gmail.com <smithjeremy3933@gmail.com>
2020-11-11 15:20:49 -0800

    first commit

---
+++ Scripts/OverworldSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CityBuilder.Overworld
+{
+    public class OverworldSelection : MonoBehaviour
+    {
+        Dictionary<Province, GameObject> provinceGameobjectMap = new Dictionary<Province, GameObject>();
+        GameObject selectedObject;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
